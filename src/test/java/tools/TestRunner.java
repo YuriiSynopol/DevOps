d35da7b460bf6ca5cff58cc67818ada7840348c9
Yurii Synopol <synopol.yuri@gmail.com>
2019-08-27 22:20:48 +0300

    Test10

--- src/test/java/tools/TestRunner.java
+++ src/test/java/tools/TestRunner.java
@@ -28,13 +28,14 @@ public abstract class TestRunner {
 
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 //        options.addArguments("start-maximized"); // open Browser in maximized mode
 //        options.addArguments("disable-infobars"); // disabling infobars
 //        options.addArguments("--disable-extensions"); // disabling extensions
 //        options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
-        options.addArguments("--headless");
+
         // options.setExperimentalOption("useAutomationExtension", false);
 
         driver = new ChromeDriver(options);
